f63213984f504b26d5f46eb7f3ddfc709772aa48
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-12-04 13:35:28 -0500

    Solve Day 1

--- src/adventofcode_2017/day1.clj
+++ src/adventofcode_2017/day1.clj
@@ -1,1 +1,57 @@
-(ns adventofcode-2017.day1)
+(ns adventofcode-2017.day1
+  (:require [clojure.string :as string]))
+
+(def captcha (string/trim-newline (slurp "resources/day1-input.txt")))
+
+(defn pairs
+  "Return a seq of pairs of a sequence.
+
+  The sequence is circular, so the digit after the last digit is the first digit
+  in the list."
+  [coll]
+  (partition 2 1 [(first coll)] coll))
+
+(defn halfway-pairs
+  "Return a seq of pairs halway around the sequence.
+
+  The sequence is circular, so the digit after the last digit is the first digit
+  in the list."
+  [captcha]
+  (let [halfway #(/ (count %) 2)
+        [left right] (split-at (halfway captcha) captcha)]
+    (map #(list %1 %2) left right)))
+
+
+(defn solve
+  "Find the sum of all digits that match the next digit, given by f, in the list."
+  [f captcha]
+  (->> (seq captcha)
+       f
+       (filter (fn [[left right]] (= left right)))
+       (map first)
+       (map #(Character/digit % 10))
+       (apply +)))
+
+(def part-1 {"1122" 3
+             "1111" 4
+             "1234" 0
+             "91212129" 9})
+
+(= (map #(solve pairs %) (keys part-1))
+   (vals part-1))
+;; => true
+
+(def part-2 {"1212" 6
+             "1221" 0
+             "123425" 4
+             "123123" 12
+             "12131415" 4})
+
+(= (map #(* (solve halfway-pairs %) 2)  ; (* 2) because the pairs are reflected
+        (keys part-2))
+   (vals part-2))
+;; => true
+
+(map #(% captcha) [#(solve pairs %)
+                   #(* (solve halfway-pairs %) 2)])
+;; => (1136 1092)
