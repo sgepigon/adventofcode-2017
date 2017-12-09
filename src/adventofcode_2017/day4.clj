37f61486f99f47f026d85ee5643096634f8d0dd1
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-12-08 22:40:02 -0500

    Add day 4 input and solutions

---
+++ src/adventofcode_2017/day4.clj
@@ -0,0 +1,46 @@
+(ns adventofcode-2017.day4
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.string :as string]
+            [orchestra.spec.test :as spec.test]))
+
+(def passphrases (string/split-lines (slurp "resources/day4-input.txt")))
+
+(spec/def ::word string?)
+(spec/def ::passphrase (spec/coll-of ::word :distinct true))
+
+(defn str->coll-of-strs
+  [s]
+  (string/split s #"\s"))
+
+(defn valid-passphrase?
+  [coll]
+  (spec/valid? ::passphrase coll))
+
+(def part-1 {"aa bb cc dd ee" true
+             "aa bb cc dd aa" false
+             "aa bb cc dd aaa" true})
+
+(= (map (comp valid-passphrase? str->coll-of-strs) (keys part-1))
+   (vals part-1))
+;; => true
+
+(frequencies (map (comp valid-passphrase? str->coll-of-strs) passphrases))
+;; => {false 175, true 337}
+
+(defn sort-letters
+  "Sort the letters of a word alphabetically"
+  [s]
+  (apply str (sort s)))
+
+(def part-2 {"abcde fghij" true
+             "abcde xyz ecdab" false
+             "a ab abc abd abf abj" true
+             "iiii oiii ooii oooi oooo" true
+             "oiii ioii iioi iiio" false})
+
+(= (map (comp valid-passphrase? #(map sort-letters %) str->coll-of-strs) (keys part-2))
+   (vals part-2))
+;; => true
+
+(frequencies (map (comp valid-passphrase? #(map sort-letters %) str->coll-of-strs) passphrases))
+;; => {false 281, true 231}
